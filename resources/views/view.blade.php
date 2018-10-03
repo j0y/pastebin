132cedb9a14afa79aea7a26fe71e32d8f7be6bfc
j0y <12984600+j0y@users.noreply.github.com>
2018-10-03 18:08:22 +0700

    add home button, change view style

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -13,27 +13,27 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-sm-11">
-                <h3>{{ $snippet->title }}</h3>
-            </div>
+        <div class="card">
+            <div class="card-header"><h3>{{ $snippet->title }}</h3></div>
 
-        </div>
-        <div class="row">
-            <div class="col-xs-12">
-                <ul class="list-inline" style="color:#999FA4;">
-                    <li>Created by: <i>{{ $username }}</i></li>
-                    <li>Created at: <i>{{ $snippet->created_at }}</i></li>
-                    <li>Expires: <i> {{(is_null($snippet->expiration)) ? "Never" : $snippet->expiration }}</i></li>
-                    <li>Access: <i> {{ $snippet->access }}</i></li>
-                    <li>Syntax: <i> {{ is_null($snippet->syntax) ? "Plain-text" : $snippet->syntax }} </i></li>
-                </ul>
-            </div>
-        </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <ul class="list-inline" style="color:#999FA4;">
+                            <li>Created by: <i>{{ $username }}</i></li>
+                            <li>Created at: <i>{{ $snippet->created_at }}</i></li>
+                            <li>Expires: <i> {{(is_null($snippet->expiration)) ? "Never" : $snippet->expiration }}</i></li>
+                            <li>Access: <i> {{ $snippet->access }}</i></li>
+                            <li>Syntax: <i> {{ is_null($snippet->syntax) ? "Plain-text" : $snippet->syntax }} </i></li>
+                        </ul>
+                    </div>
+                </div>
 
-        <div class="row" style="margin-bottom:20px;">
-            <div class="col-sm-12">
-                <pre><code class="{{ $snippet->syntax }}">{{ $snippet->code }}</code></pre>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <pre><code class="{{ $snippet->syntax }}">{{ $snippet->code }}</code></pre>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
